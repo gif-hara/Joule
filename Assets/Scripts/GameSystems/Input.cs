514ba286f46a896c12d6a4dc5d8acb179e10743e
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-05-22 17:28:03 +0900

    :new: とりあえずプレイヤー動くように

---
+++ Assets/Scripts/GameSystems/Input.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Joule.GameSystems
+{
+    /// <summary>
+    /// 入力に関するクラス
+    /// </summary>
+    public static class Input
+    {
+        public static float Horizontal
+        {
+            get { return UnityEngine.Input.GetAxis("Horizontal"); }
+        }
+        
+        public static float Vertical
+        {
+            get { return UnityEngine.Input.GetAxis("Vertical"); }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerControllers/PlayerLocomotion.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using Input = Joule.GameSystems.Input;
+
+namespace Joule.PlayerControllers
+{
+    /// <summary>
+    /// プレイヤーの移動を制御する
+    /// </summary>
+    public sealed class PlayerLocomotion : MonoBehaviour
+    {
+        [SerializeField]
+        private CharacterController characterController;
+
+        [SerializeField]
+        private float speed;
+
+        private Transform cachedTransform;
+
+        void Awake()
+        {
+            this.cachedTransform = this.transform;
+        }
+        
+        void Update()
+        {
+            var velocity =
+                (this.cachedTransform.forward * Input.Vertical + this.cachedTransform.right * Input.Horizontal)
+                .normalized;
+
+            this.characterController.SimpleMove(velocity * speed * Time.deltaTime);
+        }
+    }
+}
