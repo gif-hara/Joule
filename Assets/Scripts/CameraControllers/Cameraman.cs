e1090fd790285b5b9d21c31a99aebcaffdf5c6fd
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-05-22 17:55:39 +0900

    :new: カメラ制御できるように

---
+++ Assets/Scripts/CameraControllers/Cameraman.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Joule.CameraControllers
+{
+    /// <summary>
+    /// カメラを制御するクラス
+    /// </summary>
+    public sealed class Cameraman : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform root;
+        public Transform Root
+        {
+            get { return root; }
+        }
+
+        [SerializeField]
+        private Transform pivot;
+
+        [SerializeField]
+        private Transform dolly;
+
+        [SerializeField]
+        private Transform rig;
+
+        [SerializeField]
+        private Camera controlledCamera;
+        public Camera ControlledCamera
+        {
+            get { return controlledCamera; }
+        }
+        
+        
+    }
+}
---
+++ Assets/Scripts/CameraControllers/GameCameraController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Joule.CameraControllers
+{
+    /// <summary>
+    /// ゲーム中のカメラを制御するクラス
+    /// </summary>
+    public sealed class GameCameraController : MonoBehaviour
+    {
+        [SerializeField]
+        private Cameraman cameraman;
+
+        [SerializeField]
+        private Transform track;
+        public Transform Track
+        {
+            get { return track; }
+            set { track = value; }
+        }
+
+        [SerializeField]
+        private float chaseTrackSmoothTime;
+
+        private Vector3 chaseTrackVelocity;
+
+        void Update()
+        {
+            this.ChaseTrack();
+        }
+
+        private void ChaseTrack()
+        {
+            if (this.track == null)
+            {
+                return;
+            }
+
+            this.cameraman.Root.localPosition = Vector3.SmoothDamp(
+                this.cameraman.Root.localPosition,
+                this.track.position,
+                ref this.chaseTrackVelocity,
+                this.chaseTrackSmoothTime * Time.deltaTime
+            );
+        }
+    }
+}
