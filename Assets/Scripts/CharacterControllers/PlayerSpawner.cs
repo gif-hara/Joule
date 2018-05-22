5aee09c3d0beb93982167e09c019a74a763e360c
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-05-22 20:27:11 +0100

    :new: Edit中はイメージモデルが表示されるように

--- Assets/Scripts/CharacterControllers/PlayerSpawner.cs
+++ Assets/Scripts/CharacterControllers/PlayerSpawner.cs
@@ -3,12 +3,16 @@ using Joule.CameraControllers;
 using Joule.Events.CharacterControllers;
 using UnityEngine;
 using UnityEngine.Assertions;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace Joule.CharacterControllers
 {
     /// <summary>
     /// プレイヤーを生成するクラス
     /// </summary>
+    [ExecuteInEditMode]
     public sealed class PlayerSpawner : MonoBehaviour
     {
         [SerializeField]
@@ -25,6 +29,12 @@ namespace Joule.CharacterControllers
 
         void Awake()
         {
+#if UNITY_EDITOR
+            if(!Application.isPlaying)
+            {
+                return;
+            }
+#endif
             var t = this.transform;
             var character = Instantiate(this.character);
             character.CachedTransform.position = t.position;
@@ -34,5 +44,47 @@ namespace Joule.CharacterControllers
             Instantiate(this.cameraController);
             Broker.Global.Publish(PlayerSpawned.Get(character));
         }
+
+#if UNITY_EDITOR
+        [SerializeField]
+        private GameObject imageModel;
+
+        private void Start()
+        {
+            EditorApplication.playModeStateChanged -= this.PlayModeStateChanged;
+            EditorApplication.playModeStateChanged += this.PlayModeStateChanged;
+        }
+
+        private void Update()
+        {
+            if (Application.isPlaying)
+            {
+                return;
+            }
+
+            if (this.imageModel != null)
+            {
+                return;
+            }
+
+            if(this.model == null)
+            {
+                return;
+            }
+
+            this.imageModel = Instantiate(this.model, this.transform);
+            this.imageModel.transform.localPosition = Vector3.zero;
+            this.imageModel.transform.localRotation = Quaternion.identity;
+
+        }
+
+        private void PlayModeStateChanged(PlayModeStateChange state)
+        {
+            if(state == PlayModeStateChange.EnteredPlayMode)
+            {
+                DestroyImmediate(this.imageModel);
+            }
+        }
+#endif
     }
 }
