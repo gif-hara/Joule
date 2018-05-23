54bdf63fca7739a624aff2938374a6ebcc337053
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-05-24 02:07:27 +0900

    :new: クールタイム実装

--- Assets/Joule/Scripts/BulletControllers/MuzzleController.cs
+++ Assets/Joule/Scripts/BulletControllers/MuzzleController.cs
@@ -18,18 +18,40 @@ namespace Joule
             set { prefab = value; }
         }
 
+        [SerializeField]
+        private float coolTime;
+
         private Transform cachedTransform;
+
+        private float currentCoolTime;
 
         void Awake()
         {
             this.cachedTransform = this.transform;
+            this.currentCoolTime = this.coolTime;
+        }
+
+        void Update()
+        {
+            this.currentCoolTime += Time.deltaTime;
         }
 
         public void Fire(Character owner)
         {
+            if (!this.CanFire)
+            {
+                return;
+            }
+
+            this.currentCoolTime = 0.0f;
             var bullet = this.prefab.Rent(owner);
             bullet.CachedTransform.position = this.cachedTransform.position;
             bullet.CachedTransform.rotation = this.cachedTransform.rotation;
+        }
+
+        public bool CanFire
+        {
+            get { return this.currentCoolTime >= this.coolTime; }
         }
     }
 }
