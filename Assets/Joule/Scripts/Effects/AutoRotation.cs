f025e0b851eb735ca20d68ae3aeda3eec43cc84f
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-05-26 21:39:28 +0900

    :new: スポーナーっぽいモデル作る

---
+++ Assets/Joule/Scripts/Effects/AutoRotation.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Joule.Effects
+{
+    /// <summary>
+    /// アタッチしたオブジェクトを回転させる
+    /// </summary>
+    public sealed class AutoRotation : MonoBehaviour
+    {
+        public enum SimulationSpace
+        {
+            Local,
+            World,
+        }
+        
+        [SerializeField]
+        private float speed;
+
+        [SerializeField]
+        private Vector3 axis;
+
+        [SerializeField]
+        private SimulationSpace simulationSpace;
+        
+        private Transform cachedTransform;
+
+        void Awake()
+        {
+            this.cachedTransform = this.transform;
+        }
+        
+        void Update()
+        {
+            var t = this.speed * Time.deltaTime;
+            var value = Quaternion.AngleAxis(t, this.axis);
+            if (this.simulationSpace == SimulationSpace.Local)
+            {
+                this.cachedTransform.localRotation *= value;
+            }
+            else if (this.simulationSpace == SimulationSpace.World)
+            {
+                this.cachedTransform.rotation *= value;
+            }
+            else
+            {
+                Assert.IsTrue(false);
+            }
+        }
+    }
+}
