6819def371a89f5b3a103542a253f639fcf7dc73
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-05-26 19:26:43 +0900

    :new: 攻撃するように

--- Assets/Joule/Scripts/CharacterControllers/AI/States/Attack.cs
+++ Assets/Joule/Scripts/CharacterControllers/AI/States/Attack.cs
@@ -1,4 +1,8 @@
-﻿using UnityEngine;
+﻿using System;
+using Joule.BulletControllers;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Assertions;
 
@@ -12,6 +16,16 @@ namespace Joule.CharacterControllers.AI
     {
         [SerializeField]
         private GameObject muzzle;
+
+        [SerializeField]
+        private float interval;
+
+        [SerializeField]
+        private float rotationSmoothTime;
+
+        private MuzzleController[] muzzleControllers;
+
+        private Vector3 rotationVelocity;
         
         public override void OnEnter(AIControllerBase aiController)
         {
@@ -23,7 +37,37 @@ namespace Joule.CharacterControllers.AI
 
             navMeshAgent.enabled = false;
             navMeshObstacle.enabled = true;
-            Debug.Log("?");
+
+            aiController.UpdateAsObservable()
+                .Where(_ => aiController.isActiveAndEnabled)
+                .SubscribeWithState2(this, aiController, (_, _this, a) =>
+                {
+                    var owner = a.Owner.CachedTransform;
+                    var v = a.Target.CachedTransform.position - owner.position;
+                    v.y = 0.0f;
+                    var current = owner.rotation.eulerAngles;
+                    var target = Quaternion.LookRotation(v.normalized).eulerAngles;
+                    var t = _this.rotationSmoothTime * Time.deltaTime;
+                    owner.rotation = Quaternion.Euler(
+                        Mathf.SmoothDampAngle(current.x, target.x, ref _this.rotationVelocity.x, t),
+                        Mathf.SmoothDampAngle(current.y, target.y, ref _this.rotationVelocity.y, t),
+                        Mathf.SmoothDampAngle(current.z, target.z, ref _this.rotationVelocity.z, t)
+                    );
+                })
+                .AddTo(this.runningEvents)
+                .AddTo(aiController);
+
+            Observable.Interval(TimeSpan.FromSeconds(this.interval))
+                .Where(_ => aiController.isActiveAndEnabled)
+                .SubscribeWithState2(this, aiController, (_, _this, a) =>
+                {
+                    foreach (var m in _this.muzzleControllers)
+                    {
+                        m.Fire(a.Owner);
+                    }
+                })
+                .AddTo(this.runningEvents)
+                .AddTo(aiController);
         }
 
         public override StateBase Clone(AIControllerBase aiController)
@@ -33,6 +77,11 @@ namespace Joule.CharacterControllers.AI
             instance.muzzle.transform.SetParent(aiController.Owner.CachedTransform);
             instance.muzzle.transform.localPosition = Vector3.zero;
             instance.muzzle.transform.localRotation = Quaternion.identity;
+            instance.interval = this.interval;
+            instance.rotationSmoothTime = this.rotationSmoothTime;
+            instance.muzzleControllers = instance.muzzle.GetComponentsInChildren<MuzzleController>();
+            Assert.AreNotEqual(instance.muzzleControllers.Length, 0, string.Format("{0}に{1}が存在しません", instance.muzzle, typeof(MuzzleController)));
+
             return instance;
         }
     }
